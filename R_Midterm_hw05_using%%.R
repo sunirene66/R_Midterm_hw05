275482fd9d3e3cc95e5b01f19da94daab6f9abaf
sunirene66 <home9992002@yahoo.com.tw>
2017-02-10 20:39:19 +0800

    first commit

---
+++ R_Midterm_hw05_using%%.R
@@ -0,0 +1,23 @@
+# HW05: Using %>% to simply the Program
+# this_year <- as.numeric(format(Sys.Date(), '%Y'))
+# birth_year <- this_year - straw_hat_df$age
+# birth_date_chr <- paste(birth_year, straw_hat_df$birthday, sep = "-")
+# straw_hat_df$birth_date <- as.Date(birth_date_chr)
+
+library(magrittr)
+load(url("https://storage.googleapis.com/r_rookies/straw_hat_df.RData"))
+
+straw_hat_df$birth_date <- Sys.Date() %>%
+  format(. , '%Y') %>%
+  as.numeric(.) %>% 
+  `-` (straw_hat_df$age) %>%
+  paste(. , straw_hat_df$birthday, sep = "-") %>%
+  as.Date(.)
+
+
+
+
+
+
+
+
